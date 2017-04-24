53562ad706aeacf2b8f46e20c27a00ef45083e66
firstshort <first.short@gmail.com>
2017-04-24 10:30:22 +0100

    updated versions in pom and added geckodriver to test java

--- WebdriverTest/src/test/java/example/NewTest.java
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -8,15 +8,18 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;   
 import org.testng.annotations.AfterTest;        
 public class NewTest {      
-        private WebDriver driver;       
+        private WebDriver driver;  
+        String driverPath = "C:\\geckodriver.exe";
+        
         @Test              
-        public void testEasy() {    
+        public void testEasy() {  
             driver.get("http://www.guru99.com/selenium-tutorial.html");  
             String title = driver.getTitle();                
             Assert.assertTrue(title.contains("Free Selenium Tutorials"));       
         }   
         @BeforeTest
         public void beforeTest() {  
+        	System.setProperty("webdriver.firefox.marionette", driverPath);
             driver = new FirefoxDriver();  
         }       
         @AfterTest
@@ -24,3 +27,5 @@ public class NewTest {
             driver.quit();          
         }       
 }   
+
+
